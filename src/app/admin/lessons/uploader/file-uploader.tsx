ca781e4bed074d8f9f8ed5d550aab8c0f3d1a8f8
Bukangliwayway <jamesmatthewbelgica@gmail.com>
2025-01-17 01:19:24 +0800

    fix: improve type safety in debounce function parameters

--- src/app/admin/lessons/uploader/file-uploader.tsx
+++ src/app/admin/lessons/uploader/file-uploader.tsx
@@ -224,9 +224,12 @@ export function FileUploader({
   );
 }
 
-function debounce(fn: Function, delay: number) {
+function debounce<T extends (...args: any[]) => any>(
+  fn: T,
+  delay: number
+): (...args: Parameters<T>) => void {
   let timeoutId: NodeJS.Timeout;
-  return (...args: any[]) => {
+  return (...args: Parameters<T>) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => fn(...args), delay);
   };
